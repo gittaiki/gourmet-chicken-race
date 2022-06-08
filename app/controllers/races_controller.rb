c462f6d0ce4454aa4a85e91820d522966e2c13d5
gittaiki <0215.taiki@gmail.com>
2022-06-08 23:05:16 +0900

    Add: 設定金額を設定#7

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,6 +1,10 @@
 class RacesController < ApplicationController
   def new
-    @menus = RakutenWebService::Ichiba::Item.search(keyword: 'フランス料理').first(10)
+    @menus = RakutenWebService::Ichiba::Item.search(keyword: 'フランス料理').first(15).sample(10)
+    price = 0
+    @menus.each { |menu| price += menu['itemPrice'] * 10 }
+    num = [25, 30, 30, 35, 35, 40].sample
+    @set_price = (price / num).round(-3)
   end
 
   def show; end
